cd99e9d4e46b2c6afd31394282537fc7ea6c018c
NorskNoobing <39239702+NorskNoobing@users.noreply.github.com>
2022-08-13 17:29:38 +0200

    change filenames

---
+++ Build-Module.ps1
@@ -0,0 +1,29 @@
+#Requires -Module ModuleBuilder
+[string]$moduleName = Split-Path ".\" -LeafBase
+
+[version]$version = "0.0.1"
+[string]$author = "NorskNoobing"
+[string]$ProjectUri = "https://github.com/$author/$moduleName"
+[string]$releaseNotes = "Initial commit"
+[string]$description = "A module to update game stats from multiple games and APIs"
+[array]$tags = @("Gaming","Apex Legends","Valorant")
+[version]$PSversion = "7.2"
+
+$manifestSplat = @{
+    "Description" = $description
+    "PowerShellVersion" = $PSversion
+    "Tags" = $tags
+    "ReleaseNotes" = $releaseNotes
+    "Path" = ".\source\$moduleName.psd1"
+    "RootModule" = "$moduleName.psm1"
+    "Author" = $author
+    "ProjectUri" = $ProjectUri
+}
+New-ModuleManifest @manifestSplat
+
+$buildSplat = @{
+    "SourcePath" = ".\source\$moduleName.psd1"
+    "CopyPaths" = @("$(Get-Location)\README.md")
+    "Version" = $version
+}
+Build-Module @buildSplat
--- source/Public/Invoke-ApexTrackerggBridgeRequest.ps1
+++ source/Public/Invoke-ApexTrackerggBridgeRequest.ps1
@@ -6,19 +6,21 @@ function Invoke-TrackerggBridgeRequest {
     
     process {
         $OriginUsernames | ForEach-Object -ThrottleLimit 20 -Parallel {
-            try {
-                $splat = @{
-                    "Method" = "GET"
-                    "Uri" = "https://apex.tracker.gg/apex/profile/origin/$($_)/matches"
-                    "Headers" = @{
-                        "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
-                    }
+            $username = $_
+
+            $splat = @{
+                "Method" = "GET"
+                "Uri" = "https://apex.tracker.gg/apex/profile/origin/$username/matches"
+                "Headers" = @{
+                    "User-Agent" = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
                 }
+            }
+
+            try {
                 $null = Invoke-WebRequest @splat
             }
             catch {
                 if (($_.exception.response.statuscode) -eq "NotFound") {
-                    $username = ($_.exception.response.requestmessage.requesturi -split "/")[6]
                     throw "User $username was not found"
                 } else {
                     $_
